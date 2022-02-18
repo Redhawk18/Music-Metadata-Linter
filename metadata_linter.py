b8ebbca1919e7fc3f2b2f780eb1bf2c659920569
Redhawk18 <Redhawk76767676@gmail.com>
2022-02-18 12:43:30 -0500

    fixed function's condition, and improved print style

--- metadata_linter.py
+++ metadata_linter.py
@@ -6,6 +6,7 @@ from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
 
 
+
 def list_to_string(list):
     '''Takes a list input and returns the 0-th index'''
     string = list[0]
@@ -20,27 +21,26 @@ def string_linter(current_song, tag, tag_value):
     '''removes anything between () if it contains anything from badwords list, 
     and returns the string no matter if it was changed or not'''
 
-    bad_words = ("remastered", "Remastered", "REMASTERED",
+    bad_words = ("remastered", "Remastered", "REMASTERED", "Re-Mastered",
     "anniversary", "Anniversary", "ANNIVERSARY",
     "edition", "Edition", "EDITION",
     "version", "Version", "VERSION"
     )
+    sum = 0
 
-    index = 0
-    for bad_words in tag_value:
+    for bad_word in bad_words:
         #sees if string has bad word in it
-        if bad_words[index] in tag_value: #tag needs to be edited
+        if bad_word in tag_value: #tag needs to be edited
             #this assumes the data is structured like this
-            #title (remastered).mp3
-            #TODO find out why the if statement is true 100%
-            tmplist = list_to_string(current_song[tag]).split(' (')
-            tag_value = tmplist[0]
-            
-            return tag_value #returns edited value
+            #title (remastered).mp3 
+            sum += 1
+            return_list = (list_to_string(current_song[tag]).split(' (')[0], sum)
+            return return_list
 
-        else: #tag is fine and does not need to be edited
-            return list_to_string(current_song[tag]) #non edited
-    index += 1
+    #else
+    return_list = (tag_value, sum)
+    return return_list
+
 
 def main():
     '''takes any .mp3 in working-dir and lints the junk out of it's title and album tags and saves the song'''
@@ -55,6 +55,8 @@ def main():
 
     index = 0
     amount_of_errors = 0
+    tags_modified = 0
+
     for current_song in list_of_songs:
         index += 1 #pre increment
 
@@ -66,16 +68,22 @@ def main():
             continue
 
         #process metadata
-        title = string_linter(current_song, "title", list_to_string(current_song["title"]))
-        save_current_song(current_song, "title", title)
+        title_list = string_linter(current_song, "title", list_to_string(current_song["title"]))
+        save_current_song(current_song, "title", title_list[0])
+        tags_modified += title_list[1]
 
-        album = string_linter(current_song, "album", list_to_string(current_song["album"]))
-        save_current_song(current_song, "album", album)
+        album_list = string_linter(current_song, "album", list_to_string(current_song["album"]))
+        save_current_song(current_song, "album", album_list[0])
+        tags_modified += title_list[1]
 
-
+    
     #results
-    print(index, "files proccessed,", amount_of_errors, "errors occurred")
+    print(index, "files proccessed")
+    print(amount_of_errors, "errors occurred")
+    print(tags_modified, "tags modified")
+
     if not amount_of_errors == 0:
         print("errors can be caused by non music files in the directory")
+
 
 main()
